dd22bb5a1939c54e2e19888e2177048f20be82d3
edwbartels <edwbartels@gmail.com>
2024-10-12 01:14:44 -0400

    added custom selectors files, need to refactor to use

---
+++ frontend/src/store/selectors.js
@@ -0,0 +1,4 @@
+export const getOwner = (state) => state.spots.current?.Owner;
+export const getUser = (state) => state.session.user;
+export const getSpot = (state) => state.spots.current;
+export const getReviews = (state) => state.spots.current?.Reviews;
