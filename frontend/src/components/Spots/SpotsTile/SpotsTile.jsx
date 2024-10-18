06b85f86ccf4e8a57d882654d3b6f1c4391064a6
edwbartels <edwbartels@gmail.com>
2024-10-18 12:32:36 -0400

    last commit before final review

--- frontend/src/components/Spots/SpotsTile/SpotsTile.jsx
+++ frontend/src/components/Spots/SpotsTile/SpotsTile.jsx
@@ -11,40 +11,38 @@ const SpotsTile = ({ spot }) => {
 	};
 
 	return (
-		<div data-testid="spot-tile" className="grid-item">
-			<a onClick={handleClick}>
-				<div className="tile-image-container">
-					<img
-						data-testid="spot-thumbnail-image"
-						className="image-thumbnail"
-						// title={spot.name}
-						src={spot.previewImage || null}
-						// onClick={handleClick}
-					/>
-					<span className="spot-tooltip" data-testid="spot-tooltip">
-						{spot.name}
-					</span>
-					{/* <span data-testid="spot-tooltip" style={{ display: 'none' }}>
+		<div data-testid="spot-tile" className="grid-item" onClick={handleClick}>
+			<div className="tile-image-container">
+				<img
+					data-testid="spot-thumbnail-image"
+					className="image-thumbnail"
+					// title={spot.name}
+					src={spot.previewImage || null}
+					// onClick={handleClick}
+				/>
+				<span className="spot-tooltip" data-testid="spot-tooltip">
+					{spot.name}
+				</span>
+				{/* <span data-testid="spot-tooltip" style={{ display: 'none' }}>
 				{spot.name}
 			</span> */}
-				</div>
-				<div className="info">
-					<div className="location-rating">
-						<span>
-							<span data-testid="spot-city">{spot.city},</span> {spot.state}
-						</span>
-						<span className="star-rating">
-							<FaStar className="tile-star" />
-							<div data-testid="spot-rating">{`${
-								spot.avgRating ? spot.avgRating.toFixed(2) : 'New'
-							}`}</div>
-						</span>
-					</div>
-					<span data-testid="spot-price" className="price">
-						${spot.price}/night
+			</div>
+			<div className="info">
+				<div className="location-rating">
+					<span>
+						<span data-testid="spot-city">{spot.city},</span> {spot.state}
+					</span>
+					<span className="star-rating">
+						<FaStar className="tile-star" />
+						<div data-testid="spot-rating">{`${
+							spot.avgRating ? spot.avgRating.toFixed(2) : 'New'
+						}`}</div>
 					</span>
 				</div>
-			</a>
+				<span data-testid="spot-price" className="price">
+					${spot.price}/night
+				</span>
+			</div>
 		</div>
 	);
 };
